ac20869c7f8d92341014bd33c528ea763d58989b
Felipe Pedroso <f.pedroso677@gmail.com>
2016-08-02 14:30:22 -0300

    Updating java version and implementing a new endpoint on the interface.

--- app/src/main/java/br/pedroso/moviesretrofit/rest/MoviesService.java
+++ app/src/main/java/br/pedroso/moviesretrofit/rest/MoviesService.java
@@ -3,13 +3,17 @@ package br.pedroso.moviesretrofit.rest;
 import br.pedroso.moviesretrofit.rest.model.MovieResults;
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 
 /**
  * Created by felip on 20/07/2016.
- */
+*/
 public interface MoviesService {
     String API_KEY_QUERY_PARAMETER = "api_key";
 
     @GET("movie/upcoming")
     Call<MovieResults> listUpcomingMovies();
+
+    @GET("movie/{movieId}/similar")
+    Call<MovieResults> listSimilarMovies(@Path("movieId") Integer movieId);
 }
